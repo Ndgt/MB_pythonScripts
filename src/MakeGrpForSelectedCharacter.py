d88359bcf61ced82f0b178af66001248d7939ce9
Ndgt <nadesubber2@gmail.com>
2024-05-21 21:06:00 +0900

    change MakeGrpForSelectedCharacter.py

--- src/MakeGrpForSelectedCharacter.py
+++ src/MakeGrpForSelectedCharacter.py
@@ -1,15 +1,16 @@
-import pyfbsdk
-from pyfbsdk import FBApplication, FBFindModelByLabelName, FBGroup, FBPropertyListObject
+from pyfbsdk import FBApplication, FBGroup, FBMessageBox
 
 chara = FBApplication().CurrentCharacter
-
 group = FBGroup("CharacterSkeleton")
 
 for prop in chara.PropertyList:
-    if type(prop) == pyfbsdk.FBPropertyListObject:
-        if len(prop) > 0 and not "Solver" in prop.Name:
+    if not prop.Name.find("Link") == -1:
+        if len(prop) > 0:
             for j in range(len(prop)):
-                if type(prop[j]) == pyfbsdk.FBModelSkeleton:
-                    group.ConnectSrc(prop[j])
+                group.ConnectSrc(prop[j])
 
-del(chara, group, FBApplication, FBFindModelByLabelName, FBGroup, FBPropertyListObject)
+
+if len(group.Items) > 0:
+    FBMessageBox("Character Successfully Confirmed","Check group in Resources Window >> Groups","OK")
+
+del(chara, group,FBApplication,FBGroup,FBMessageBox)
