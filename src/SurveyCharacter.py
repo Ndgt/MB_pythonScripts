910e0fe89963346318d999d88617c34af4a0a9d6
Ndgt <nadesubber2@gmail.com>
2024-05-27 20:43:13 +0900

    set list

--- src/SurveyCharacter.py
+++ src/SurveyCharacter.py
@@ -3,48 +3,55 @@ from pyfbsdk import*
 try:
     chara = FBApplication().CurrentCharacter
 
-    SkeletonNodeIdList = list(vars(FBSkeletonNodeId)["values"].values())
-    for id in SkeletonNodeIdList:
-        model = chara.getModel(id)
-
-    MeshList = list(vars(FBBodyNodeId)["values"].values())
-    for id in MeshList:
-        model = chara.GetModel(id)
-        if not model == None:
-            print(model.Name)
-
-    def BoneNum(chara):
-        returnNum = 0
-        for prop in chara.PropertyList:
-            if prop.Name.endswith("Link") and len(prop) > 0:
-                returnNum += 1
-        return str(returnNum)
-
-    def isSetReference(chara):
-        prop = chara.PropertyList.Find("ReferenceLink")
-        if len(prop) > 0:
-            return "set"
-        else:
-            return "not set"
-        
-    def MakeGroup(chara):
-        group = FBGroup("CharacterSkeleton")
-        for prop in chara.PropertyList:
-            if prop.Name.endswith("Link") and len(prop) > 0:
-                group.ConnectSrc(prop[0])
-        if len(group.Items) > 0:
-            return True
+    if not chara == None:
+        def BoneNum(chara):
+            returnNum = 0
+            for prop in chara.PropertyList:
+                if prop.Name.endswith("Link") and len(prop) > 0:
+                    returnNum += 1
+            return str(returnNum)
 
 
-    FBMessageBox("Result", 
-                "Bone number : " + BoneNum(chara) + "\n" \
-                "Reference : " + isSetReference(chara),
-                "OK")
+        def isSetReference(chara):
+            prop = chara.PropertyList.Find("ReferenceLink")
+            if len(prop) > 0:
+                return "set"
+            else:
+                return "not set"
 
-    if MakeGroup(chara):
-        FBMessageBox("Result","Character bone group Created.\n\nCheck it in Resources Window >> Groups","OK")
+
+        def MakeCharacterSkeletonGroup(chara):
+            group = FBGroup("CharacterSkeleton")
+            for prop in chara.PropertyList:
+                if prop.Name.endswith("Link") and len(prop) > 0:
+                    group.ConnectSrc(prop[0])
+
+        '''
+        # below code is also serve as making skeleton group
+        def MakeCharacterSkeletonGroup2(chara):
+            group = FBGroup("CharacterSkeleton")            
+            BodyNodeIdList = list(vars(FBBodyNodeId)["values"].values())
+            for id in BodyNodeIdList:
+                model = chara.GetModel(id)
+                if not model == None:
+                    group.ConnectSrc(model)
+        '''
+
+        def MakeCharacterMeshGroup(chara):
+            group = FBGroup("ChracterMesh")
+            mList = FBModelList()
+            for mesh in chara.GetSkinModelList(mList):
+                group.ConnectSrc(mesh)
 
 
+
+        FBMessageBox("Result", 
+                    "Bone number : " + BoneNum(chara) + "\n" \
+                    "Reference : " + isSetReference(chara),
+                    "OK")
+
+        if Group(chara):
+            FBMessageBox("Result","Character Skeleton group / Mesh group Created.\nCheck it in Resources Window >> Groups","OK")
 
 except:
     FBMessageBox("Cauton", "Error : Select a Charater", "OK")
